422ea08815733b189c0c331e10bea38604683b86
Juan Sebastián Herrera Molano <juanseherreram14@gmail.com>
2022-12-12 08:57:51 -0500

    final

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace JuanHerrera_ExamenP2;
 
@@ -11,18 +12,49 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-    void OnValoresRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
+    private void OnValoresRadioButtonCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        Label label = new Label { Text = "Usted ha ecogido una recarga de " + e.ToString() + " dolares"};
+        if(v3.IsChecked== true) {
+            mensaje.Text = $"Usted ha seleccionado una recarga de {t3.Value} dolares";
+        }
+        if (v5.IsChecked == true)
+        {
+            mensaje.Text = $"Usted ha seleccionado una recarga de {t5.Value} dolares";
+        }
+        if (v10.IsChecked == true)
+        {
+            mensaje.Text = $"Usted ha seleccionado una recarga de {t10.Value} dolares";
+        }
+
     }
 
     private async void OnButtonClicked(object sender, EventArgs e)
     {
-        bool answer = await DisplayAlert("Confirmacion", "Desea Hacer la recarga?", "Si", "No");
-        if (answer)
+        if (v3.IsChecked == true)
         {
-            await DisplayAlert("Confirmado", "Tu recarga ha sido exitosa", "OK");
+            bool answer = await DisplayAlert("Confirmacion", $"Desea Hacer la recarga de {v3.Value}", "Si", "No");
+            if (answer)
+            {
+                await DisplayAlert("Confirmado", "Tu recarga ha sido exitosa", "OK");
+            }
         }
+        if (v5.IsChecked == true)
+        {
+            bool answer = await DisplayAlert("Confirmacion", $"Desea Hacer la recarga de {v5.Value}", "Si", "No");
+            if (answer)
+            {
+                await DisplayAlert("Confirmado", "Tu recarga ha sido exitosa", "OK");
+            }
+        }
+        if (v10.IsChecked == true)
+        {
+            bool answer = await DisplayAlert("Confirmacion", $"Desea Hacer la recarga de {v10.Value}", "Si", "No");
+            if (answer)
+            {
+                await DisplayAlert("Confirmado", "Tu recarga ha sido exitosa", "OK");
+            }
+        }
+       
     }
 }
 
