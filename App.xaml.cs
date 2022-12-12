4ac819be07e3609b7dff71792128a5c4b908cc4d
juans <juanseherreram14@gmail.com>
2022-12-12 07:27:44 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JuanHerrera_ExamenP2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JuanHerrera_ExamenP2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
